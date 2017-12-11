4b1f4edcbf0442b709281be61301be866927d061
Filip Cizkovsky <filip.cizkovsky@economia.cz>
2017-12-11 14:17:28 +0100

    update gallery to autoscroll, fux error in observer

--- galerie.php
+++ galerie.php
@@ -14,52 +14,36 @@ $images = glob($dirname."*.JPG");
 var images = <?php echo json_encode($images); ?>;
 const ln = images.length;
 
-let index = 0;
+let count = 0, allImgLoaded = false;
 
-if (!index) loadImages(index);
+if (!count) loadImages(count);
 
 function loadImages(index) {
 	for (let i = index; i <= ln; i++ ) {
 
 		if (images[i]) {
 			displayImage(images[i]);
-			//console.log(images[i]);
 		}
 		
 		if ( i == ln ) {
-			console.info('END');
-			removeOldButton();
+			allImgLoaded = true;
+			//console.info('END');
+			//removeOldButton();
 			return;
 		}
 
-		if (i == index + 9) {
-			console.log('kuk');
-			addButton(i+1);
+		if (i == index + 5) {  // tady resim kolik nacist dalsich obrazku
+			//console.log('kuk');
+			//loadMore(i + 1);
+			//addButton(i+1);
+			count = i + 1;
 			return;
 		}
 	}
 }
 
 
-function addButton(i){
-	removeOldButton();
 
-	const button = document.createElement('button');
-	button.id = "more";
-	button.textContent = '...další...';
-	button.className += 'button-more ';
-	button.addEventListener('click', function() {
-		loadImages(i);
-	});	
-	document.body.appendChild(button);
-}
-
-function removeOldButton() {
-	const oldBtn = document.getElementById("more");
-	if (oldBtn) {
-		oldBtn.parentNode.removeChild(oldBtn);
-	}
-}
 
 function displayImage(src) {
 	const img = document.createElement('img');
@@ -75,7 +59,7 @@ function addZpetButton(i){
 	button.textContent = 'nahoru';
 	button.className += 'button-zpet ';
 	button.addEventListener('click', function() {
-		scrollToTop(200);
+		window.scrollTo(0,0);
 	});	
 	document.body.appendChild(button);
 }
@@ -83,24 +67,68 @@ function addZpetButton(i){
 window.addEventListener('scroll', checkScroll);
 
 function checkScroll (e) {
-	console.log(e);
+	//console.log(e);
 	if (window.scrollY > 300) {
 		addZpetButton();
 		window.removeEventListener('scroll', checkScroll);
 	}
 }
 
-function scrollToTop(scrollDuration) {
-    var scrollStep = -window.scrollY / (scrollDuration / 15),
-        scrollInterval = setInterval(function(){
-        if ( window.scrollY != 0 ) {
-            window.scrollBy( 0, scrollStep );
-        }
-        else clearInterval(scrollInterval); 
-    },15);
+
+window.onscroll = function() {
+  var d = document.documentElement;
+  var offset = d.scrollTop + window.innerHeight;
+  var height = d.offsetHeight;
+
+  // console.log('offset = ' + offset);
+  // console.log('height = ' + height);
+
+  if (offset === height && !allImgLoaded) {
+    // console.log('At the bottom');
+
+    /* vyhod observer na reklamu hazi chybu */
+	if (observer) {
+		observer.disconnect();
+		observer = false;
+	} 
+	/* loadni dalsi images kdyz doscrolluju dolu  */
+    loadImages(count);
+  } else {
+  	return;
+  }
+};
+
+
+/*  button more nepouzito  
+
+function removeOldButton() {
+	const oldBtn = document.getElementById("more");
+	if (oldBtn) {
+		oldBtn.parentNode.removeChild(oldBtn);
+	}
 }
 
 
+function addButton(i){
+	removeOldButton();
+
+	const button = document.createElement('button');
+	button.id = "more";
+	button.textContent = '...další...';
+	button.className += 'button-more ';
+	button.addEventListener('click', function() {
+		// vyhod observer na reklamu hazi chybu 
+		if (observer) {
+			observer.disconnect();
+			observer = false;
+		} 
+
+		loadImages(i);
+	});	
+	document.body.appendChild(button);
+}
+
+*/
 
 </script>
 
--- includes/footer.php
+++ includes/footer.php
@@ -2,19 +2,21 @@
 <script type="text/javascript">
 /* vyhod tu reklamu */
 
- var foo = document.querySelector('body');
+   var foo = document.querySelector('body');
 
   var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-      // console.log('mutation.type = ' + mutation.type);
-      // console.log(mutation);
+      mutations.forEach(function(mutation) {
+        // console.log('mutation.type = ' + mutation.type);
+        // console.log(mutation);
 
-      if (mutation.type = "childList" && mutation.addedNodes[0].nodeName == "DIV" && mutation.addedNodes[0].outerHTML.match('000webhost')) {
-        //console.log(mutation.addedNodes[0]);
-        mutation.addedNodes[0].style.cssText = "display: none !important;";
-      }
-    });
+        if (mutation.type = "childList" && mutation.addedNodes[0].nodeName == "DIV" && mutation.addedNodes[0].outerHTML.match('000webhost')) {
+          //console.log(mutation.addedNodes[0]);
+          if (mutation.addedNodes[0])  mutation.addedNodes[0].style.cssText = "display: none !important;";
+        }
+      }); 
   });
+
+
   observer.observe(foo, { childList: true });
   
 </script>
